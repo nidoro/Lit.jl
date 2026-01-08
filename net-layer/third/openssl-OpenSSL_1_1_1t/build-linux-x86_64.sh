6f208675c036c0bb3e112d30afa09f9a39255f07
Davi Doro <ni.doro@gmail.com>
2026-01-07 22:55:59 -0300

    Cross compilation of openssl targeting windows

--- net-layer/third/openssl-OpenSSL_1_1_1t/build-linux-x86_64.sh
+++ net-layer/third/openssl-OpenSSL_1_1_1t/build-linux-x86_64.sh
@@ -2,7 +2,7 @@
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-DEST_DIR=$THIS_DIR/../../../local/build/openssl-OpenSSL_1_1_1t
+DEST_DIR=$THIS_DIR/../../../build/linux-x86_64/openssl-OpenSSL_1_1_1t
 mkdir -p $DEST_DIR
 
 ./config --prefix=$DEST_DIR
